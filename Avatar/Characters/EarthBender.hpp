fd3646696d302721a37933c217ec6182eb4f9355
Menden Cannistra <mendenc@hawaii.edu>
2026-04-26 00:08:17 -1000

    Update EarthBender.hpp

--- Avatar/Characters/EarthBender.hpp
+++ Avatar/Characters/EarthBender.hpp
@@ -4,26 +4,30 @@
 //
 /// @file    EarthBender.hpp
 /// @author  Edward Felipe III <efelipe3@hawaii.edu>
-/// EarthBender is a derived class of PlayerCharacter. EarthBenders use earth manipulation to fight enemies and protect allies.
+/// EarthBender is a derived class of PlayerCharacter.
+/// EarthBenders use earth manipulation to fight enemies and protect allies.
 /////////////////////////////////////////////////////////////////////////////////
+
 #ifndef EARTHBENDER_HPP
 #define EARTHBENDER_HPP
 #include "PlayerCharacter.hpp"
 #include "../BendingSystem/Earth_Bending.hpp" // CORRECT INCLUDE PATH
+#include <string>
 
 /// EarthBender: a profession derived from PlayerCharacter that specializes in earth manipulation
 class EarthBender : public PlayerCharacter {
     private:
         // Pointer to EarthBending system for performing earth bending actions
-        EarthBending* earthBendingSystem; 
+        EarthBending* earthBendingSystem;
         
     public:
         /// Constructor:
-        explicit EarthBender(string& characterName, int& raceCode);
+        explicit EarthBender(std::string& characterName, int& raceCode);
         // Destructor: Essential cleanup when using raw pointers
         ~EarthBender();
+        
         /// use bending system to perform an earth bending action (REMOVED 'override' KEYWORD)
-    virtual void performAction(PlayerCharacter& target);
+        virtual void performAction(PlayerCharacter& target);
         
         /// Print EarthBender-specific stats in addition to base stats
         void printStats();
