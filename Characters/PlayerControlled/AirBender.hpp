92ade94549498199de08fb2d55f457dec47f3d88
Menden Cannistra <mendenc@hawaii.edu>
2026-04-26 23:05:42 -1000

    also revert

--- Characters/PlayerControlled/AirBender.hpp
+++ Characters/PlayerControlled/AirBender.hpp
@@ -1,9 +1,9 @@
 ///////////////////////////////////////////////////////////////////////////////
-//  University of Hawaii, College of Engineering
-//  Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
+//  University of Hawaii, College of Engineering
+//  Lab 13b - Game Character Class Part III (Polymorphism) - ECE 205 - Spring 2026
 //
-/// @file    AirBender.hpp
-/// @author  Edward Felipe III <efelipe3@hawaii.edu>
+/// @file    AirBender.hpp
+/// @author  Edward Felipe III <efelipe3@hawaii.edu>
 /// AirBender is a derived class of PlayerCharacter. AirBenders use air manipulation to fight enemies and protect allies.
 /////////////////////////////////////////////////////////////////////////////////
 #ifndef AIRBENDER_HPP
@@ -14,24 +14,23 @@
 
 /// AirBender: a profession derived from PlayerCharacter that specializes in air manipulation
 class AirBender : public PlayerCharacter {
-    private:
-        // Pointer to AirBending system for performing air bending actions
-        AirBending* airBendingSystem;
-        
-    public:
-        /// Constructor:
-explicit AirBender(const std::string& characterName, const int& raceCode);
-
-        // Destructor: Essential cleanup when using raw pointers
-        ~AirBender();
-        
-        /// use bending system to perform an air bending action (REMOVED 'override' KEYWORD)
-        virtual void performAction(FighterCharacter& target);
-        
-        /// Print AirBender-specific stats in addition to base stats
-        void printStats();
-        
-        /// AirBender-specific greeting
-        void greet() const override;
+    private:
+        // Pointer to AirBending system for performing air bending actions
+        AirBending* airBendingSystem;
+        
+    public:
+        /// Constructor:
+        explicit AirBender(std::string& characterName, int& raceCode);
+        // Destructor: Essential cleanup when using raw pointers
+        ~AirBender();
+        
+        /// use bending system to perform an air bending action (REMOVED 'override' KEYWORD)
+        virtual void performAction(FighterCharacter& target);
+        
+        /// Print AirBender-specific stats in addition to base stats
+        void printStats();
+        
+        /// AirBender-specific greeting
+        void greet() const override;
 };
 #endif //AIRBENDER_HPP
